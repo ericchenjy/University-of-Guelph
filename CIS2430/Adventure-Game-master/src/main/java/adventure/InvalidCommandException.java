9734f4cf288402d5a4d07815a9dd323a6a73646c
Eric Chen <45884650+ericchenjy@users.noreply.github.com>
2021-07-27 11:19:32 -0400

    Delete CIS2430 directory

--- CIS2430/Adventure-Game-master/src/main/java/adventure/InvalidCommandException.java
+++
@@ -1,7 +0,0 @@
-package adventure;
-public class InvalidCommandException extends Exception{
-    
-    InvalidCommandException(String msg){
-        super(msg);
-    }
-}
--- CIS2430/Final-Grade-Calculator-master/src/main/java/gradecalc/ResearchSubject.java
+++
@@ -1,8 +0,0 @@
-package gradecalc;
-
-public interface ResearchSubject {
-    
-    public void setParticipating(boolean isParticipating);
-    public double researchPerk();  //returns the bonus grade
-
-}
